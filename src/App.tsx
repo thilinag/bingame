1545c4c55005f0c54e85aacc406405a0876590ad
Thilina Gunasekara <thilina.gunasekara@gmail.com>
2023-11-11 00:01:28 +1030

    add prettier, fix input movement logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect, useReducer, useRef } from "react";
+import { useState, useCallback, useEffect, useRef } from "react";
 import classNames from "classnames";
 import "./App.css";
 
@@ -40,115 +40,150 @@ import { useStore } from "./hooks/useStore";
 export const HERO_SIZE = 64;
 
 function App() {
-  const state = useStore((state) => state);
-  const { move, downPressed, setHero, setStage, stuff } = state;
-  const [time, setTime] = useState(0);
-  const stageRef = useRef(null);
-  const heroRef = useRef(null);
+    const state = useStore((state) => state);
+    const {
+        setHero,
+        setStage,
+        stuff,
+        press,
+        rightPressed,
+        leftPressed,
+        upPressed,
+        downPressed,
+        posX,
+        posY,
+    } = state;
+    const [time, setTime] = useState(0);
+    const stageRef = useRef(null);
+    const heroRef = useRef(null);
 
-  useLoop((time: number) => {
-    setTime(time);
-  }, state);
+    useLoop((time: number) => {
+        const {
+            moveUp,
+            moveDown,
+            moveLeft,
+            moveRight,
+            downPressed,
+            upPressed,
+            leftPressed,
+            rightPressed,
+        } = state;
+        if (downPressed) {
+            moveDown();
+        }
+        if (upPressed) {
+            moveUp();
+        }
+        if (leftPressed) {
+            moveLeft();
+        }
+        if (rightPressed) {
+            moveRight();
+        }
+        setTime(time);
+    }, state);
 
-  const handleKeyPress = useCallback((event: KeyboardEvent) => {
-    const { key } = event;
-    switch (key) {
-      case "w":
-        move("up");
-        break;
+    const handleKeyPress = useCallback((event: KeyboardEvent) => {
+        const { key } = event;
+        switch (key) {
+            case "w":
+                press("up");
+                break;
 
-      case "a":
-        move("left");
+            case "a":
+                press("left");
 
-        break;
+                break;
 
-      case "s":
-        move("down");
-        break;
+            case "s":
+                press("down");
+                break;
 
-      case "d":
-        move("right");
-        break;
+            case "d":
+                press("right");
+                break;
 
-      default:
-        break;
-    }
-  }, []);
+            default:
+                break;
+        }
+    }, []);
 
-  const handleKeyRelease = useCallback((event: KeyboardEvent) => {
-    const { key } = event;
-    switch (key) {
-      case "w":
-        move("up", false);
-        break;
+    const handleKeyRelease = useCallback((event: KeyboardEvent) => {
+        const { key } = event;
+        switch (key) {
+            case "w":
+                press("up", false);
+                break;
 
-      case "a":
-        move("left", false);
-        break;
+            case "a":
+                press("left", false);
+                break;
 
-      case "s":
-        move("down", false);
-        break;
+            case "s":
+                press("down", false);
+                break;
 
-      case "d":
-        move("right", false);
-        break;
+            case "d":
+                press("right", false);
+                break;
 
-      default:
-        break;
-    }
-  }, []);
+            default:
+                break;
+        }
+    }, []);
 
-  useEffect(() => {
-    document.addEventListener("keydown", handleKeyPress);
-    document.addEventListener("keyup", handleKeyRelease);
-    if (stageRef.current) {
-      setStage(stageRef.current);
-    }
-    if (heroRef.current) {
-      setHero(heroRef.current);
-    }
+    useEffect(() => {
+        document.addEventListener("keydown", handleKeyPress);
+        document.addEventListener("keyup", handleKeyRelease);
+        if (stageRef.current) {
+            setStage(stageRef.current);
+        }
+        if (heroRef.current) {
+            setHero(heroRef.current);
+        }
 
-    return () => {
-      document.removeEventListener("keydown", handleKeyPress);
-      document.removeEventListener("keyup", handleKeyRelease);
-    };
-  }, []);
+        return () => {
+            document.removeEventListener("keydown", handleKeyPress);
+            document.removeEventListener("keyup", handleKeyRelease);
+        };
+    }, []);
 
-  return (
-    <main ref={stageRef} className="stage">
-      <div
-        ref={heroRef}
-        className={classNames("hero", {
-          moving_right: state.rightPressed,
-          moving_left: state.leftPressed,
-          moving_up: state.upPressed,
-          moving_down: state.downPressed,
-        })}
-        style={{
-          left: state.posX,
-          top: state.posY,
-          width: HERO_SIZE,
-        }}
-      ></div>
-      {stuff.map(({ x, y, width, height, background, name }) => (
-        <div
-          key={name}
-          className="thing"
-          style={{
-            left: `${x}px`,
-            top: `${y}px`,
-            height: `${height}px`,
-            width: `${width}px`,
-            background,
-          }}
-        >
-          {name}
-        </div>
-      ))}
-      colliding: {state.colliding_with}
-    </main>
-  );
+    return (
+        <main ref={stageRef} className="stage">
+            <div
+                ref={heroRef}
+                className={classNames("hero", {
+                    moving_right: rightPressed,
+                    moving_left: leftPressed,
+                    moving_up: upPressed,
+                    moving_down: downPressed,
+                })}
+                style={{
+                    left: posX,
+                    top: posY,
+                    width: HERO_SIZE,
+                }}
+            ></div>
+            {stuff.map(
+                ({ x, y, width, height, background, name, isColliding }) => (
+                    <div
+                        key={name}
+                        className="thing"
+                        style={{
+                            left: `${x}px`,
+                            top: `${y}px`,
+                            height: `${height}px`,
+                            width: `${width}px`,
+                            background,
+                            ...(isColliding && { outline: "1px solid red" }),
+                        }}
+                    >
+                        {name}
+                    </div>
+                )
+            )}
+        </main>
+    );
 }
 
 export default App;
